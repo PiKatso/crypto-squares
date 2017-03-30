08d0e74ed129514685af8eadd8865c47996e6ce2
Kai Leahy and Kat Gifford <kai@>
2017-03-30 14:57:51 -0700

    create site for encrypting strings using square encryption

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+var translateString = function(string) {
+  var numChars = string.length;
+  var charArray = string.split('');
+  var columns = Math.floor(Math.sqrt(numChars));
+  var numberOfRows = (string.length % (columns ** 2)) ? columns + 1 : columns;
+  var bigArray = [];
+  while (charArray.length > 0) {
+    bigArray.push(charArray.splice(0, columns));
+  }
+  var cryptoArray = [];
+  for(var i = 0; i < columns; i++) {
+    bigArray.forEach(function(row) {
+      cryptoArray.push(row[i]);
+    });
+  }
+  return cryptoArray.join('');
+}
+
+var validateString = function(string) {
+  return true;
+}
+
+$(document).ready(function() {
+  $('form').submit(function(){
+    var input = $('input').val();
+    if (validateString(input) === true) {
+      //compute result
+      var result = translateString(input);
+      $('#output').text(result);
+    }
+    return false;
+  });
+});
